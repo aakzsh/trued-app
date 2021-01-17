0a697512d77fe183f088d25d9aaab26101bf4394
joeyy <job.madi@protonmail.com>
2021-01-17 19:35:38 +0200

    minor changes

--- lib/Data/Api.dart
+++ lib/Data/Api.dart
@@ -1,4 +1,3 @@
-
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
@@ -6,32 +5,21 @@ import 'package:http/http.dart' as http;
 import 'data.dart';
 
 class Api {
-
-
   Api({
     this.url,
   });
- 
-String url;
- 
 
-Future<Data> factchecks() async {
-  final response =
-      await http.get(url);
+  String url;
 
-  if (response.statusCode == 200) {
-    // If the server did return a 200 OK response,
-    // then parse the JSON.
-    print('success');
-    print(response.body);
-    print('hello check');
-    return Data.fromJson(jsonDecode(response.body));
-  } else {
-    // If the server did not return a 200 OK response,
-    // then throw an exception.
-    throw Exception('Failed to load Results');
+  Future<Data> factchecks() async {
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      print('success');
+     
+      return Data.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to load Results');
+    }
   }
-
-
-}
 }
--- lib/main_drawer.dart
+++ lib/main_drawer.dart
@@ -1,32 +1,23 @@
-
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
 
 final datenow = new DateTime.now();
 
 class MainDrawer extends StatelessWidget {
-
   @override
-
   Widget build(BuildContext context) {
     return Drawer(
-
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
           Container(
             decoration: BoxDecoration(
-
               shape: BoxShape.rectangle,
-              borderRadius: BorderRadius.vertical(
-                  bottom: Radius.circular(40.0)),
-             
+              borderRadius:
+                  BorderRadius.vertical(bottom: Radius.circular(40.0)),
             ),
             width: double.infinity,
             padding: EdgeInsets.all(10.0),
-
             child: Center(
               child: Column(
                 children: <Widget>[
@@ -35,9 +26,11 @@ class MainDrawer extends StatelessWidget {
                     height: 100.0,
                     decoration: BoxDecoration(
                       shape: BoxShape.circle,
-                      image: DecorationImage(image: AssetImage( //firebase se linked karna hai vro
-                        'assets/logo.png',
-                      ),
+                      image: DecorationImage(
+                        image: AssetImage(
+                          //firebase se linked karna hai vro
+                          'assets/logo.png',
+                        ),
                         fit: BoxFit.fill,
                       ),
                     ),
@@ -52,133 +45,103 @@ class MainDrawer extends StatelessWidget {
                       ),
                     ),
                   ),
-
-
                 ],
               ),
             ),
-
           ),
-
-
           Align(
             alignment: Alignment.centerLeft,
             child: FlatButton.icon(
               onPressed: () {},
-
               icon: Icon(
                 Icons.settings,
-
               ),
-              label: Text("Settings",
+              label: Text(
+                "Settings",
                 style: TextStyle(
                   fontSize: 15.0,
                   color: Colors.grey[900],
                 ),
               ),
             ),
-
-          ),
-
-          Align(
-            alignment: Alignment.centerLeft,
-            child: FlatButton.icon(
-              onPressed: () {
-
-
-              },
-
-              icon: Icon(
-                Icons.library_books,
-
-              ),
-              label: Text("Privacy Policy",
-                style: TextStyle(
-                  fontSize: 15.0,
-                  color: Colors.grey[900],
-                ),
-              ),
-            ),
-
           ),
           Align(
             alignment: Alignment.centerLeft,
             child: FlatButton.icon(
               onPressed: () {},
-
               icon: Icon(
                 Icons.library_books,
-
               ),
-              label: Text("Terms and Conditions",
+              label: Text(
+                "Privacy Policy",
                 style: TextStyle(
                   fontSize: 15.0,
                   color: Colors.grey[900],
                 ),
               ),
             ),
-
           ),
           Align(
             alignment: Alignment.centerLeft,
             child: FlatButton.icon(
               onPressed: () {},
-
+              icon: Icon(
+                Icons.library_books,
+              ),
+              label: Text(
+                "Terms and Conditions",
+                style: TextStyle(
+                  fontSize: 15.0,
+                  color: Colors.grey[900],
+                ),
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: FlatButton.icon(
+              onPressed: () {},
               icon: Icon(
                 Icons.keyboard,
-
               ),
-              label: Text("Resources We Use",
+              label: Text(
+                "Resources We Use",
                 style: TextStyle(
                   fontSize: 15.0,
                   color: Colors.grey[900],
                 ),
               ),
             ),
-
           ),
-
           Align(
             alignment: Alignment.centerLeft,
             child: FlatButton.icon(
               onPressed: () {},
-
               icon: Icon(
                 Icons.account_circle,
-
               ),
-              label: Text("LogOut",
+              label: Text(
+                "LogOut",
                 style: TextStyle(
                   fontSize: 15.0,
                   color: Colors.grey[900],
                 ),
               ),
             ),
-
           ),
-
           Container(
-
-            height: 80, width: double.infinity,
+            height: 80,
+            width: double.infinity,
             decoration: BoxDecoration(
-            
               shape: BoxShape.rectangle,
               borderRadius: BorderRadius.vertical(top: Radius.circular(40.0)),
-
             ),
             child: Center(
-
               child: Padding(
-
                 padding: EdgeInsets.all(10.0),
                 child: Text(
-
-
                   '$datenow',
-                  style: TextStyle(
-                      fontSize: 15.0,
-                      color: Colors.white
-                  ),
+                  style: TextStyle(fontSize: 15.0, color: Colors.white),
                 ),
               ),
             ),
@@ -187,13 +150,13 @@ class MainDrawer extends StatelessWidget {
       ),
     );
   }
-  void func(BuildContext context){
+
+  void func(BuildContext context) {
     final scaffold = Scaffold.of(context);
-    scaffold.showSnackBar(SnackBar(
-      content: const Text("It will be available soon!"),
-    ),
+    scaffold.showSnackBar(
+      SnackBar(
+        content: const Text("It will be available soon!"),
+      ),
     );
   }
-
-
 }
